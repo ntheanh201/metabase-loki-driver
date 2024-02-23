d5144fae752baa7aa440349d5cbe00f5acd52967
The Anh Nguyen <ntheanh201@gmail.com>
2024-02-23 17:15:12 +0700

    lein init project

---
+++ test/metabase_loki_driver/core_test.clj
@@ -0,0 +1,7 @@
+(ns metabase-loki-driver.core-test
+  (:require [clojure.test :refer :all]
+            [metabase-loki-driver.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
